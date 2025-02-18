ea4aaf57b6a283df36b06d2bb8d035ffc4363e6f
Park Yoonseo <patpark97@gmail.com>
2025-02-18 15:59:46 +0900

    init: spring boot 초기 세팅 중간 세이브

--- src/main/java/com/example/guestbook/repository/GuestBookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.guestbook.repository;
-
-import com.example.guestbook.entity.GuestBookMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GuestBookRepository extends JpaRepository<GuestBookMessage, Long> {
-}
